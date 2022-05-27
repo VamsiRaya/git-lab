3d79808fb0c9f0044e5fd43bd85080430fc84000
vidyanandinividya <vidyanandinividya@gmail.com>
2022-05-27 19:58:21 +0530

    division

--- calculator-nive.java
+++ calculator-nive.java
@@ -7,3 +7,9 @@ public int sub(int a,int b){
 public int mul(int a,int b){
     return a*b;
 }
+public int div(int a,int b){
+    return a/b;
+}
+public int sum(int a,int b,int c){
+    return a+b+c;
+}
